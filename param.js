2fb0c3d08ce6eee1734a6f39360c07ac2f61fb72
sidcatkr <82746617+sidcatkr@users.noreply.github.com>
2025-07-09 14:29:30 +0900

    parameter 배움

---
+++ param.js
@@ -0,0 +1,23 @@
+function plus(){
+    return 1 + ''
+}
+
+console.log(plus(2))
+// 아마도 3 나올듯
+// 아님. '' 라는 빈 *문자열*이라서 1만 출력됨.
+
+function plus2(){
+    return '' + ''
+}
+
+console.log(plus2(3))
+// 아마도 6 나올듯
+// 아님. 빈 문자열끼리 더해서 안됨.
+
+function plus3(a, b){
+    return a + b
+}
+
+console.log(plus3(1, 3))
+// 아마도 4 나올듯
+// 이건 잘함.
